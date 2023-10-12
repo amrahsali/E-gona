d8dd5a91e09922efbe9d1324b0026250a50e5058
amrah <Saliamrah300@gmail.com>
2023-10-12 23:00:06 +0100

    first push

---
+++ app/src/main/java/com/example/e_gona/Login.java
@@ -0,0 +1,23 @@
+package com.example.e_gona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class Login extends AppCompatActivity {
+
+
+    Button button;
+    private EditText userNameEdt, passwordEdt;
+    //private FirebaseAuth mAuth;
+    private ProgressBar loadingPB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
