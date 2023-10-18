f04daec1173ce483efce340d640a9faffcd41ab6
tradetify <tradetify0@gmail.com>
2023-10-18 19:49:17 +0100

    Aamir's update

---
+++ app/src/main/java/com/example/e_gona/AddCalenderActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_gona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCalenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_calender);
+    }
+}
