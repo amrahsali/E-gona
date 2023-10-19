4ef0cf0e4d5988b6dfaf3318bb9a8772c7d75ca1
tradetify <tradetify0@gmail.com>
2023-10-19 21:16:23 +0100

    Aamir's update

--- app/src/main/java/com/example/e_gona/MainActivity.java
+++ app/src/main/java/com/example/e_gona/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.e_gona;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 import Model.CalendarModel;
@@ -23,33 +34,27 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ImageView profilebtn;
     FloatingActionButton fab;
-
+    FirebaseFirestore db;
+    ArrayList<CalendarModel> calendar;
+    SharedPreferences sharedpreferences;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedpreferences = getSharedPreferences("myPref", Context.MODE_PRIVATE);
+
         profilebtn = (ImageView) findViewById(R.id.profile_item);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         recyclerView = (RecyclerView) findViewById(R.id.recylerview);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
         CalendarAdapter adapter = new CalendarAdapter(MainActivity.this);
-        ArrayList<CalendarModel> calendar = new ArrayList<>();
-        CalendarModel c = new CalendarModel();
-        c.Name = "Beans";
-        c.Description = "Planted this to harvest within a short period of time";
-        c.DatePlanted = "02/05/2021";
-        c.HarvestDate = "16 days left";
-        c.Status = "Not harvested";
-        CalendarModel c1 = new CalendarModel();
-        c1.Name = "PawPaw";
-        c1.Description = "Saw an apportunity to not only enjoy the fruit myself but also to sell it when costly.";
-        c1.DatePlanted = "02/05/2021";
-        c1.HarvestDate = "5 days left";
-        c1.Status = "planted";
-        calendar.add(c);
-        calendar.add(c1);
+        calendar = new ArrayList<>();
+
+        db = FirebaseFirestore.getInstance();
+
+        getData();
         adapter.setCalendar(calendar);
         recyclerView.setAdapter(adapter);
 
@@ -68,5 +73,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    public void getData(){
+        String email = sharedpreferences.getString("email_key", null);
+        db.collection("Calendar")
+                .whereEqualTo("UserId",email)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                                calendar.add(documentSnapshot.toObject(CalendarModel.class));
+                                documentSnapshot.get("CompetionDate");
+                            }
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Error Loading Data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
     }
 }
